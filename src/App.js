b320a3af3de2b7f66264922c3ce5991a1547c4bd
danurrizad <danurrizad242@gmail.com>
2023-01-20 15:23:51 +0700

    add parameter for components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import {useState} from 'react';
 import './App.css';
 import Header from './components/header';
 import ContentMint from './components/contentmint';
 
 function App() {
+  const [isConnected, setIsConnected] = useState(false);
   return (
     <div className="App">
-      <Header/>
-      <ContentMint/>
+      <Header isConnected={isConnected} setIsConnected={setIsConnected}/>
+      <ContentMint isConnected={isConnected} setIsConnected={setIsConnected}/>
     </div>
   );
 }
